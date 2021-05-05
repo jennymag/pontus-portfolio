8601f1ce01b869b82fad4a26665d38ea2a05dea1
Jenny Magnessen <jennymagnessen@hotmail.com>
2021-05-05 16:54:54 +0200

    fix about css

--- src/About/About.js
+++ src/About/About.js
@@ -1,16 +1,21 @@
 function About() {
   return (
     <section id="about">
-      <h1 className="aboutH1">
-        Hello I’m <span className="yellowColor">Pontus</span>
-      </h1>
-      <p className="aboutP">
-        A curious and energic UX designer in the making. When a team put their
-        creative minds together and come up with innovative solutions that is as
-        visually appealing as they are easy to use, is truly one of the most
-        satisfying things. My current goal is to dive deeper and learn more
-        about the world of UX!
-      </p>
+      <div className="aboutWrapper">
+        <h1 className="aboutH1">
+          Hello I’m <span className="yellowColor">Pontus</span>
+        </h1>
+        <p className="aboutP">
+          A curious and energic UX designer in the making. When a team put their
+          creative minds together and come up with innovative solutions that is
+          as visually appealing as they are easy to use, is truly one of the
+          most satisfying things. My current goal is to dive deeper and learn
+          more about the world of UX!
+        </p>
+      </div>
+      <div className="aboutCard"></div>
+      <div className="aboutBlueCircle"></div>
+      <div className="aboutYellowCircle"></div>
     </section>
   );
 }
